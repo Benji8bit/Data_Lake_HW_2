3fe332139eb852dea017a79e76aff1e8484c5259
Benji8bit <coin8bit@hotmail.com>
2023-11-20 21:25:26 +0300

    Fixed encoding for open method

--- mapper.py
+++ mapper.py
@@ -1,6 +1,6 @@
 ### Mapper for cards.csv and requests.csv ###
 
-with open('cards.csv') as f:
+with open('cards.csv', encoding='UTF-8') as f:
     for i, line in enumerate(f):
         suit, value = line.strip().split(',')
         print(suit, value)
